5770e7ff291388146da4df108d28e9adee7ed378
RagulMJ <98083899+RagulMJ@users.noreply.github.com>
2022-10-04 23:58:21 +0530

    image removed

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
-
 export const headerData = {
-    name: 'RAGUL PRAVEEN',
-    title: "Hello! I am",
-    desciption: "Hello! I am RAGUL PRAVEEN, A passionate programmer a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ",
-    image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
-    imagebw: 'https://i.ibb.co/Cw2Xp90/abu-said-bw.jpg',
-    resumePdf: 'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing'
-}
+  name: 'RAGUL PRAVEEN',
+  title: 'Hello! I am',
+  desciption:
+    'Hello! I am RAGUL PRAVEEN, A passionate programmer a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. ',
+  image: 'https://i.ibb.co/ZmdNH6x/abu-said.jpg',
+  imagebw: '',
+  resumePdf:
+    'https://docs.google.com/document/d/1OmxnrxHbrfQkL7e3BG40HgSmYCWzNSxo9Wa-kOPVPSc/edit?usp=sharing',
+};
